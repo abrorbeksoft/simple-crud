4642a928e8f5f65531150e27c6c900ad1206de8e
abrorbek_ubaydullayev <abrorbek.workemail@gmail.com>
2022-10-14 22:37:58 +0500

    add login and registration api

--- index.js
+++ index.js
@@ -1,34 +1,82 @@
-
 const users = []
 
-const express  = require("express");
+const express = require("express");
 const app = express();
 const port = 3000;
+const bcrypt = require("bcrypt")
+
+const {v4} = require('uuid');
 
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
-router.get('/', function (req, res){
-    res.json({ "name":"Abrorbek"})
+const privateKey = "shhhhh"
+
+router.get('/', function (req, res) {
+    res.json({"name": "Abrorbek"})
 })
 
-router.get("/login", function (req, res){
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 
+router.post("/register", function (req, res) {
+    console.log("Registering...")
+
+    let {login, password} = req.body;
+
+    if (!(login && password)) {
+        res.status(400).send("All input is required");
+        return
+    }
+
+    bcrypt.hash(password, 8).then(resp => {
+        let user = {
+            "id": v4(),
+            "login": login,
+            "password": resp,
+        }
+
+        users.push(user)
+
+        let token = jwt.sign(user, privateKey);
+
+        res.json({"token": token, "message": "Login successful"});
+    })
 });
 
 
+router.post("/login", function (req, res) {
 
-router.get("/register", function (req, res){
+    const {login, password} = req.body;
 
+    if (!(login && password)) {
+        res.status(400).send("All input is required");
+        return
+    }
 
-    res.json(req.body)
+    let user = users.find((e) => {
+        return e.login === login
+    })
 
-//    user =  users.find((elem)=>{
-//     return elem.login === req.
-//     })
+    if (!user) {
+        res.json({"code": 403, "message": "Login or password incorrect"});
+        return
+    } else {
+        bcrypt.compare(password, user.password).then(res => {
+            if (!res) {
+                res.json({"code": 403, "message": "Login or password incorrect"});
+                return
+            }
+        })
+    }
+
+    let token = jwt.sign(user, privateKey);
+
+    res.json({"token": token, "message": "Login successful"});
 });
 
 app.use("/api", router)
 
-app.listen((port), function() {
+app.listen((port), function () {
     console.log("listening on port " + port);
 });
